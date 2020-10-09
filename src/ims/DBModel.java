950b97c2930bacf7672e9f0f48eaf315fedf5b9e
bilal <bilalalam173@gmail.com>
2020-10-09 23:10:54 +0500

    DBModel interface added

---
+++ src/ims/DBModel.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ims;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javafx.collections.ObservableList;
+
+/**
+ *
+ * @author bilal
+ * @param <T>
+ */
+public interface DBModel<T> {
+    ObservableList<T> getAll();
+    T getOne(Long id);
+    void deleteOne(Long id) throws SQLException;
+    void deleteAll();
+    void updateOne(T item);
+    void updateMultiple(ArrayList<T> items);
+}
